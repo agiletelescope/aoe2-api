1b3297a8c1f28c88628f0973ab51f3068d3dda5b
Agiletelescope <agiletelescope@gmail.com>
2021-10-24 21:08:57 +0530

    updates to age enum with string parser

--- aoe2_api/models/age.py
+++ aoe2_api/models/age.py
@@ -16,6 +16,32 @@ class Age(Enum):
     CASTLE = 2
     IMPERIAL = 3
 
+    mapping = {
+        DARK: "Dark",
+        FEUDAL: "Feudal",
+        CASTLE: "Castle",
+        IMPERIAL: "Imperial"
+    }
+
+    def get_str_repr(self) -> list:
+        """
+        Return all possible string representation of the current age
+
+        :return: List of all strings that are a valid
+                 representation of the current age
+        """
+
+        mapping = {
+            self.DARK: "dark",
+            self.FEUDAL: "feudal",
+            self.CASTLE: "castle",
+            self.IMPERIAL: "imperial"
+        }
+        value = mapping[self] if self in mapping else ""
+
+        # Example, when dark age, return ["dark", "Dark", "DARK"]
+        return [value, value.title(), value.upper()]
+
     def is_dark(self) -> bool:
         return self == self.DARK
 
@@ -27,3 +53,23 @@ class Age(Enum):
 
     def is_imperial(self) -> bool:
         return self == self.IMPERIAL
+
+    @staticmethod
+    def from_str(value: str):
+        """
+        Parse string to Age value
+
+        :param value: String, denoting the civilization age
+        :return: Age if parse successful, None otherwise
+        """
+        if value in Age.DARK.get_str_repr():
+            return Age.DARK
+        elif value in Age.FEUDAL.get_str_repr():
+            return Age.FEUDAL
+        elif value in Age.CASTLE.get_str_repr():
+            return Age.CASTLE
+        elif value in Age.IMPERIAL.get_str_repr():
+            return Age.IMPERIAL
+
+        # No match found
+        return
--- aoe2_api/shared/config.py
+++ aoe2_api/shared/config.py
@@ -5,3 +5,8 @@ Default App Configurations
 
 # Cost Configs
 MAX_RESOURCE_LIMIT = 10000
+
+
+# Data Parser
+DATA_DELIMITER = ","
+DATA_COST_DELIMITER = ";"
